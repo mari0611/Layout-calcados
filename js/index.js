cdb39cc67ffe35d71d0fdab5e69abbfbd1b4e3b9
mari0611 <maariana.reis@hotmail.com>
2019-10-07 17:24:15 -0300

    Primeiro commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.querySelector("nav>button").addEventListener (
+    "click",
+
+    function() {
+        let ul = document.querySelector("nav>ul");
+
+        ul.style.height = "400px"
+    }
+)
